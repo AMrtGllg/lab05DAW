f401a59d1980bfd1a65f8d21c7360de61bee80b6
AMrtGllg <amartelg@unsa.edu.pe>
2026-05-14 23:45:44 -0500

    Ejercicios 2a y 2b completados

--- Ejercicio2a.py
+++ Ejercicio2a.py
@@ -1,2 +1,9 @@
 from interpreter import draw
 from chessPictures import *
+
+fila_sup = knight.join(knight.negative())
+fila_inf = knight.negative().join(knight)
+
+figura_a = fila_sup.under(fila_inf)
+
+draw(figura_a)
--- Ejercicio2b.py
+++ Ejercicio2b.py
@@ -1,2 +1,9 @@
 from interpreter import draw
 from chessPictures import *
+
+fila_sup = knight.join(knight.negative())
+fila_inf = knight.negative().verticalMirror().join(knight.verticalMirror())
+
+figura_b = fila_sup.under(fila_inf)
+
+draw(figura_b)
